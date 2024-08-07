b64b377cbca79cc2c74e88c8a8bce037d1ae5ff7
Zhihao Zhan <zhanzhihao_dt@163.com>
2024-08-07 18:26:52 +0800

    init repo.

---
+++ include/bspline.h
@@ -0,0 +1,17 @@
+/**
+ * \file bspline.h
+ * \author Zhihao Zhan (zhanzhihao_dt@163.com)
+ * \brief bspline
+ * \version 0.1
+ * \date 2024-08-07
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef BSPLINE_H
+#define BSPLINE_H
+
+
+
+#endif // BSPLINE_H
---
+++ include/bspline_surface.h
@@ -0,0 +1,17 @@
+/**
+ * \file bspline_surface.h
+ * \author Zhihao Zhan (zhanzhihao_dt@163.com)
+ * \brief bspline拟合的表面模型
+ * \version 0.1
+ * \date 2024-08-07
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef BSPLINE_SURFACE_H
+#define BSPLINE_SURFACE_H
+
+
+
+#endif // BSPLINE_SURFACE_H
